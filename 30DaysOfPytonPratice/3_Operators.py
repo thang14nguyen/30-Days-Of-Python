8b5ac85885f87130a030e24e7bbedeec97521b05
Thang Nguyen <Thang14nguyen@gmail.com>
2023-08-31 15:20:14 -0700

    day 3 problems

--- 30DaysOfPytonPratice/3_Operators.py
+++ 30DaysOfPytonPratice/3_Operators.py
@@ -19,3 +19,13 @@ a, b, c = int(input('Enter side A: ')), int(input('Enter side B: ')), int(input(
 triangle_perimeter = sum([a, b, c])
 print('The perimeter for the triangle is ' + str(triangle_perimeter))
 
+# Problem 6:
+# Get length and width of a rectangle using prompt. Calculate its area
+# (area = length x width) and perimeter (perimeter = 2 x (length + width))
+
+rect_length, rect_height = int(input('Enter rectangle length: ')), int(input('Enter rectange height: '))
+rect_area = rect_height * rect_length
+rect_perimeter = 2 * rect_length * rect_height
+print('The area of the rectangle is ' + str(rect_area))
+print('The perimeter of the rectangle is ' + str(rect_perimeter))
+
